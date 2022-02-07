7f13ff294a9904affe83e30ab57f5281fd7f9d1b
kakaprodo <promskakaprodo45@gmail.com>
2022-02-07 15:27:23 +0200

    User login: implemented api endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\Api\LoginController;
 use App\Http\Controllers\Auth\Api\RegisterUserController;
 
 /*
@@ -16,6 +17,8 @@ use App\Http\Controllers\Auth\Api\RegisterUserController;
 */
 
 Route::post('register', [RegisterUserController::class, 'store']);
+
+Route::post('login', [LoginController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
