b2fe3d21adfee439d0f5c65ea0e1cd56810cf065
kakaprodo <promskakaprodo45@gmail.com>
2022-02-07 13:39:05 +0200

    Discount setting - seeded product discount value

---
+++ database/seeders/DiscountSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Discount;
+use Illuminate\Database\Seeder;
+
+class DiscountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Discount::truncate();
+        
+        $discounts = [
+            [
+                'description' => 'Price 50 - 100 give 0% discount',
+                'value' => 0
+            ],
+            [
+                'description' => 'Price 112 - 115 give 0.25% discount',
+                'value' => 0.25
+            ],
+            [
+                'description' => 'Discount for above 120 price give 0.50% discount',
+                'value' => 0.50
+            ],
+        ];
+
+        foreach ($discounts as $key => $discount) {
+            Discount::create($discount);
+        }
+    }
+}
