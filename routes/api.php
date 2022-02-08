ac2c7bee29e96e58a86da12436734c873cdc7ac9
kakaprodo <promskakaprodo45@gmail.com>
2022-02-08 20:47:11 +0200

    Api discounts: built an api endpoint that provide all discounts settings

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\DiscountController;
 use App\Http\Controllers\Api\PurchaseController;
 use App\Http\Controllers\Auth\Api\LoginController;
 use App\Http\Controllers\Api\TransactionController;
@@ -42,3 +43,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::resource('products', ProductController::class)
     ->only(['index', 'show']);
+
+Route::resource('discounts', DiscountController::class)
+    ->only(['index']);
